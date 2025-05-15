99a466d4e088aa6577562d174cd821bcb31f8687
ecascavita17@unisalle.edu.co <ecascavita17@unisalle.edu.co>
2025-05-15 15:43:17 -0500

    feat: new feacture port

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 # ---------------------------------------------------------------------------------------------------
 # librerias
+import os
 from dash import Dash
 from layout import create_layout
 # ---------------------------------------------------------------------------------------------------
@@ -16,4 +17,5 @@ app.layout = create_layout(app)
 # ---------------------------------------------------------------------------------------------------
 # llamado de funcion principal
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=True,host='0.0.0.0', port=port)
